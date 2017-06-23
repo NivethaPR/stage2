9f24d67edcb54664dfed9f88745227d34c145576
NivethaPR <nivethapalanisamy.be@gmail.com>
2017-06-23 10:55:25 +0530

    Add files via upload

--- PrimeNumbers.java
+++ PrimeNumbers.java
@@ -1,14 +1,34 @@
-package guvi;
+package Guvi;
 import java.util.Scanner;
 public class PrimeNumbers {
 
 	public static void main(String[] args) {
-		int i;
-		for(i=1;i<=2;i++){//loop for take 1 to 100 numbers
-		if(i%2!=0)//condition to find odd numbers between 1 to 100
-		{
-		System.out.println(i);// print odd values between 1 to 100
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		int num1, num2;
+		System.out.println("Please enter the first number");
+		num1 = sc.nextInt();
+		System.out.println("Please enter the Second number");
+		num2 = sc.nextInt();
+		System.out.println("Prime number: ");
+		for (int i=num1; i <= num2; i++ ){
+		int j;
+		for (j=2; j<i; j++){
+		int n = i%j;
+		if (n==0){
+		break;
 		}
 		}
+		if(i == j){
+		System.out.print(" "+i);
 		}
 		}
+		System.out.println();
+		}
+		
+
+
+	}
+
+
