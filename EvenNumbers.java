6ed807ec6576e19b75389d8d83a833711d3c31ba
NivethaPR <nivethapalanisamy.be@gmail.com>
2017-06-23 10:38:33 +0530

    Add files via upload

--- EvenNumbers.java
+++ EvenNumbers.java
@@ -4,7 +4,13 @@ public class EvenNumbers {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int start = 100;
+		int end = 200;
 
-	}
+		for(int i = start; i <=end; i++){
+		if( i % 2 == 0 ){
+		System.out.println("even num "+i);
+		}
+		}
 
-}
+
